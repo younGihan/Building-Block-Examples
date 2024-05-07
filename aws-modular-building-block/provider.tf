e479626df86be055817af8d8f5b40a81659b587d
yhkim <yhkim@meshcloud.io>
2024-05-07 10:36:25 +0200

    adding AWS IAM Role provider

--- aws-modular-building-block/provider.tf
+++ aws-modular-building-block/provider.tf
@@ -8,8 +8,21 @@ terraform {
   }
 }
 
+# Provider with AWS IAM User
+# provider "aws" {
+#   region      = "eu-central-1"
+#   access_key = var.access_key
+#   secret_key = var.secret_key
+# }
+
+# Provider with Assume AWS IAM Role
+# In this case the IAM User set in the environment will try to assume the specified role
+# Therefore the same IAM user as for the Backend is used to roll out the serivce
+# role_arn can also be set as environment variable AWS_ROLE_ARN
 provider "aws" {
-  region      = "eu-central-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  assume_role {
+    role_arn     = "arn:aws:iam::${var.target_aws_account_id}:role/${var.assume_aws_role_name}"
+    session_name = var.session_name
+    external_id  = var.external_id
+  }
 }
--- aws-modular-building-block/var.tf
+++ aws-modular-building-block/var.tf
@@ -1,38 +1,63 @@
+# Provider Variable
+## Assume IAM role provider
+variable "assume_aws_role_name" {
+  description = "AWS IAM Role name which the will be assumed to access the target AWS Account"
+  type        = string
+  default     = "building-block-access-role"
+}
+
+variable "external_id" {
+  description = "IAM Role extneral ID which is used for the trust relationship"
+  type        = string
+}
+variable "session_name" {
+  description = "AWS Provider Session Name"
+  type        = string
+}
+
+## IAM User provider
+# variable "access_key" {
+#   description = "Access Key for Provider"
+#   type        = string
+# }
+
+# variable "secret_key" {
+#   description = "Secret Key for Provider"
+#   type        = string
+# }
+
+# varialble and configuration
+variable "target_aws_account_id" {
+  description = "Target AWS Account ID where the service will be rolled out"
+  type        = string
+}
+variable "bucket_name" {
+  description = "Name of the bucket"
+  type        = string
+  default     = "test-tf-bucket-1231234-2"
+}
+variable "UUID" {
+  description = "uuid"
+  type        = string
+}
+
+
+# modules
 variable "enable_hub" {
   description = "If set to true, enable module a"
   type        = bool
-  default = false
+  default     = true
 }
 
 variable "enable_module_a" {
   description = "If set to true, enable module a"
   type        = bool
-  default = false
+  default     = false
 }
 
 variable "enable_module_b" {
   description = "If set to true, enable module b"
   type        = bool
-  default = false
+  default     = false
 }
 
-variable "bucket_name" {
-  description = "Name of the bucket"
-  type        = string
-  default = "test-tf-bucket-1231234-2"
-}
-
-variable "access_key" {
-  description = "Access Key for Provider"
-  type        = string
-}
-
-variable "secret_key" {
-  description = "Secret Key for Provider"
-  type        = string
-}
-
-variable "UUID" {
-  description = "uuid"
-  type        = string
-}
