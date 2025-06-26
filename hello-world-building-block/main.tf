103ba9dd136b188f28be1c52bc31ec4e97bf219b
Young-Hwan <yhkim@meshcloud.io>
2025-06-26 10:54:39 +0200

    adding bb run id

--- hello-world-building-block/main.tf
+++ hello-world-building-block/main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "example" {
   triggers = {
     example_string   = var.example_string
-    buildingBlockRun = var.bb-run
+    buildingBlockRun = var.meshstack_building_block_run_id
   }
 }
--- hello-world-building-block/var.tf
+++ hello-world-building-block/var.tf
@@ -2,8 +2,3 @@ variable "example_string" {
   type = string
   description = "Example String"
 }
-
-variable "bb-run" {
-  type = string
-  description = "building block run ID"
-}
